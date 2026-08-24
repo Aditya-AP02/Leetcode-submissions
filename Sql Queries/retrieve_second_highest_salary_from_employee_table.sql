d0e4cce71beba1dab894202bb66fa98574c95410
Aditya-AP02 <apugadeaditya@gmail.com>
2026-08-24 18:05:08 +0530

    Add employee details for second highest salary

--- Sql Queries/retrieve_second_highest_salary_from_employee_table.sql
+++ Sql Queries/retrieve_second_highest_salary_from_employee_table.sql
@@ -3,3 +3,11 @@
 select max(salary) as secondhighestsalary
 from employee
 where salary < (select max(salary) from employee)
+
+--if you want rowa also then use this 
+
+select * from employee
+where salary = (select max(salary) 
+                from employee
+                where salary < (select max(salary) 
+                from employee));
